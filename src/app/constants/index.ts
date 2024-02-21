3f60bfadf9998016b63974f2751265b91dfa038c
Lukasz <l.korkus1997@gmail.com>
2024-02-21 16:37:15 +0100

    Constants

---
+++ src/app/constants/index.ts
@@ -0,0 +1,30 @@
+interface IPages {
+  label: string;
+  path: string;
+}
+export const PAGES: IPages[] = [
+  {
+    label: 'Osiągnięcia',
+    path: 'osiagniecia',
+  },
+  {
+    label: 'Przemiany',
+    path: 'przemiany',
+  },
+  {
+    label: 'Galeria',
+    path: 'galeria',
+  },
+  {
+    label: 'O mnie',
+    path: 'o-mnie',
+  },
+  {
+    label: 'Umów trening',
+    path: 'umow-trening',
+  },
+  {
+    label: 'Kontakt',
+    path: 'kontakt',
+  },
+];
