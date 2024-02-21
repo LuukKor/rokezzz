454c560fe116cbdf6dd5bda461cea3880e3f5141
Lukasz <l.korkus1997@gmail.com>
2024-02-21 19:28:19 +0100

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [
-    provideClientHydration()
+  imports: [
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeaderComponent,
   ],
+  providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PAGES } from '../../constants';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { LogoComponent } from '../logo/logo.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    LogoComponent,
+    RouterLink,
+    MatMenuModule,
+    RouterLinkActive,
+    MatIconModule,
+  ],
   standalone: true,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   protected readonly PAGES = PAGES;
+  private bpSubscription: Subscription = new Subscription();
+  isSmallScreen = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  private bpObserve() {
+    this.bpSubscription = this.breakpointObserver
+      .observe([Breakpoints.XSmall, Breakpoints.Small])
+      .subscribe((result) => {
+        this.isSmallScreen = result.matches;
+      });
+  }
+
+  ngOnInit() {
+    this.bpObserve();
+  }
+
+  ngOnDestroy() {
+    this.bpSubscription.unsubscribe();
+  }
 }
