4f08f4f9d9be9bf79e8847b7f4a582b6b7117075
Lukasz <l.korkus1997@gmail.com>
2024-02-22 15:51:07 +0100

    Header rwd, active link, variables.scscs

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  signal,
+  ViewEncapsulation,
+} from '@angular/core';
 import { PAGES } from '../../constants';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { LogoComponent } from '../logo/logo.component';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { Subscription } from 'rxjs';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-header',
@@ -23,22 +31,31 @@ import { Subscription } from 'rxjs';
     MatMenuModule,
     RouterLinkActive,
     MatIconModule,
+    MatSidenavModule,
+    MatFormFieldModule,
   ],
   standalone: true,
+  encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   protected readonly PAGES = PAGES;
   private bpSubscription: Subscription = new Subscription();
-  isSmallScreen = false;
+  private isSmallScreen = signal(false);
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+  ) {}
   private bpObserve() {
     this.bpSubscription = this.breakpointObserver
       .observe([Breakpoints.XSmall, Breakpoints.Small])
       .subscribe((result) => {
-        this.isSmallScreen = result.matches;
+        this.isSmallScreen.set(result.matches);
       });
+  }
+
+  get getIsSmallScreen() {
+    return this.isSmallScreen();
   }
 
   ngOnInit() {
