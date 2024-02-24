868dc71240026b421158e0a85ad501bd3ef65210
Lukasz <l.korkus1997@gmail.com>
2024-02-24 13:26:28 +0100

    Footer

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { FOOTER_COLS, CONTACT_DATA } from '../../constants';
+import { NgForOf } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { AddSpacesEveryNPipe } from '../../pipes/add-spaces-every-n.pipe';
+import { MatIconModule } from '@angular/material/icon';
+import { ResponsiveService } from '../../services/responsive.service';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    MatGridListModule,
+    MatListModule,
+    NgForOf,
+    RouterLink,
+    MatButtonModule,
+    AddSpacesEveryNPipe,
+    MatIconModule,
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {
+  protected readonly FOOTER_COLS = FOOTER_COLS;
+  protected readonly CONTACT_DATA = CONTACT_DATA;
+}
