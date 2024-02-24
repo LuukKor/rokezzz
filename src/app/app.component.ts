100f20149434f898b4f5fbba9e6147bb4abea40d
Lukasz <l.korkus1997@gmail.com>
2024-02-24 13:28:02 +0100

    Enable service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ResponsiveService } from './services/responsive.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Rokezzz';
+  constructor(private _responsiveService: ResponsiveService) {}
+
+  ngOnInit(): void {
+    this._responsiveService.breakpointSubscribe();
+  }
+
+  ngOnDestroy(): void {
+    this._responsiveService.breakpointSubscription.unsubscribe();
+  }
 }
