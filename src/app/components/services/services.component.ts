ebd41bf9bb5dcb7d8c83dafb211ee70b615d5def
Lukasz <l.korkus1997@gmail.com>
2024-02-25 15:53:33 +0100

    Services section

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [MatTabsModule, MatCardModule, MatButtonModule],
+  imports: [
+    MatTabsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    NgOptimizedImage,
+  ],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss',
 })
